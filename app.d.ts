b11b585e8650199e0b3c8dafb8c513a8a17817b0
yelskcol <yelskcol@proton.me>
2022-06-18 19:18:14 +0100

    created base classes

--- app.d.ts
+++ app.d.ts
@@ -1,21 +1,67 @@
-declare enum Tokens {
-    X = 0,
-    O = 1
+declare enum PlayerTokens {
+    O = 0,
+    X = 1
+}
+interface playerArgs {
+    name: string;
+    token: PlayerTokens;
+}
+interface board {
+    1: [""];
+    2: [""];
+    3: [""];
 }
 declare class Player {
-    name: string;
-    token: string;
-    constructor();
+    private _name;
+    private _token;
+    private _wins;
+    constructor(_name: string, _token: PlayerTokens | "");
+    get name(): string;
+    get token(): "" | PlayerTokens;
+    get wins(): number;
+}
+declare class PlayerLogic {
+    private static instance;
+    private _players;
+    private constructor();
+    static getInstance(): PlayerLogic;
+    get players(): Player[];
+    private createPlayer;
+    private addPlayer;
 }
 declare class GameLogic {
+    private static instance;
     private _boardState;
-    constructor();
+    private _activePlayer;
+    private constructor();
     get boardState(): string[];
+    get activePlayer(): PlayerTokens;
+    set activePlayer(player: PlayerTokens);
     set boardState(state: string[]);
-    private createBoard;
+    boardParser(): string[];
+    private initializeBoard;
+    static getInstance(): GameLogic;
 }
 declare class DisplayController {
+    private static instance;
+    private _gameContainer;
+    private _cells;
+    constructor();
+    get container(): HTMLElement;
+    get cells(): Element[];
+    static getInstance(): DisplayController;
+    mapIdsToCells(ids: string[]): void;
+}
+declare class DOMHelper {
+    static get(target: string): HTMLElement;
+    static getAll(target: string): Element[];
 }
 declare class App {
-    static init(): void;
+    private logic;
+    private players;
+    private display;
+    private static instance;
+    private constructor();
+    static init(): App;
+    setupIds(): void;
 }
--- app.ts
+++ app.ts
@@ -1,43 +1,180 @@
-enum Tokens {
-  X,
+enum PlayerTokens {
   O,
+  X,
+}
+
+interface playerArgs {
+  name: string;
+  token: PlayerTokens;
+}
+
+interface board {
+  1: [""];
+  2: [""];
+  3: [""];
 }
 
 class Player {
-  public name: string;
-  public token: string;
-  constructor() {
-    this.name = "a";
-    this.token = "b";
+  private _wins = 0;
+  constructor(private _name: string, private _token: PlayerTokens | "") {
+    this._name = _name;
+    this._token = _token;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  get token() {
+    return this._token;
+  }
+
+  get wins() {
+    return this._wins;
+  }
+}
+
+class PlayerLogic {
+  private static instance: PlayerLogic;
+  private _players: Player[] = [];
+  private constructor() {
+    this.addPlayer(this.createPlayer());
+  }
+
+  static getInstance() {
+    if (PlayerLogic.instance) {
+      return this.instance;
+    }
+    this.instance = new PlayerLogic();
+    return this.instance;
+  }
+
+  get players() {
+    return this._players;
+  }
+
+  private createPlayer() {
+    const p = new Player("Sam", PlayerTokens.X);
+    return p;
+  }
+
+  private addPlayer(p: Player) {
+    this._players.push(p);
   }
 }
 
 class GameLogic {
-  private _boardState: string[];
-  constructor() {
-    this._boardState = [""];
-    this.boardState = this.createBoard();
+  private static instance: GameLogic;
+  private _boardState: string[] = [];
+  private _activePlayer: PlayerTokens = PlayerTokens.O;
+  private constructor() {
+    this.boardState = this.initializeBoard();
   }
 
   get boardState() {
     return this._boardState;
   }
+
+  get activePlayer() {
+    return this._activePlayer;
+  }
+
+  set activePlayer(player: PlayerTokens) {
+    this._activePlayer = player;
+  }
+
   set boardState(state: string[]) {
     this._boardState = state;
   }
 
-  private createBoard() {
+  boardParser() {
+    const b = this.boardState;
+    const r = [];
+    for (let i = 0; i < b.length; i++) {
+      for (let j = 0; j < b.length; j++) {
+        const pos = `${i}${j}`;
+        r.push(pos);
+      }
+    }
+    return r;
+  }
+
+  private initializeBoard() {
     const arr = new Array(3).fill(Array(3).fill(""));
     return arr;
   }
+
+  static getInstance() {
+    if (GameLogic.instance) {
+      return this.instance;
+    }
+    this.instance = new GameLogic();
+    return this.instance;
+  }
 }
 
-class DisplayController {}
+class DisplayController {
+  private static instance: DisplayController;
+  private _gameContainer = DOMHelper.get(".grid");
+  private _cells = DOMHelper.getAll(".grid__cell");
+  constructor() {}
+
+  get container() {
+    return this._gameContainer;
+  }
+
+  get cells() {
+    return this._cells;
+  }
+
+  static getInstance() {
+    if (DisplayController.instance) {
+      return this.instance;
+    }
+
+    this.instance = new DisplayController();
+    return this.instance;
+  }
+
+  mapIdsToCells(ids: string[]) {
+    this.cells.forEach((cell, idx) => {
+      cell.id = ids[idx];
+    });
+  }
+}
+
+class DOMHelper {
+  static get(target: string) {
+    const el = document.querySelector(target) as HTMLElement;
+    return el;
+  }
+
+  static getAll(target: string) {
+    const els = document.querySelectorAll(target);
+    const elsArr = [...els];
+    return elsArr;
+  }
+}
 
 class App {
+  private logic = GameLogic.getInstance();
+  private players = PlayerLogic.getInstance();
+  private display = DisplayController.getInstance();
+  private static instance: App;
+  private constructor() {
+    this.setupIds();
+  }
+
   static init() {
-    const logic = new GameLogic();
-    logic.boardState = [""];
+    if (App.instance) {
+      return this.instance;
+    }
+    this.instance = new App();
+    return this.instance;
+  }
+
+  setupIds() {
+    this.display.mapIdsToCells(this.logic.boardParser());
   }
 }
 
